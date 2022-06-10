5251332553c0dc5ff01fee7f2a86f5cca7d4e1c1
timo_macbookair <timoj.schmelzle@gmail.com>
2022-06-10 19:12:21 +0200

    Anpassung

--- src/MVCController.java
+++ src/MVCController.java
@@ -15,6 +15,33 @@ public class MVCController {
         //Erzeugung des Datenmodells durch ein Objekt der Klasse MVCModel
         this.model = new MVCModel();
 
+        testeRezeptverwaltung();
+
     }
+
+    public void testeRezeptverwaltung(){
+
+        Zutat zitrone = new Zutat("Zitrone", 1.00);
+        Zutat orange = new Zutat("Orange", 1.50);
+        Zutat minze = new Zutat("Minze", 0.75);
+        Zutat zucker = new Zutat("Zucker", 0.25);
+        Zutat wasser = new Zutat("Wasser", 0.10);
+        Zutat eis = new Zutat("Eis", 0.50);
+        Zutat limette = new Zutat("Limette", 2.00);
+
+        Zutat[] zutatenOrangenlimo = {orange, minze, wasser, eis};
+        Limonade orangenlimo = new Limonade("Orangenlimo", zutatenOrangenlimo, false,true, "Orange", true);
+
+        Zutat[] zutatenZitronenlimo = {zitrone, minze, zucker, wasser, eis};
+        Limonade zitronenlimo = new Limonade("Zitronenlimo", zutatenZitronenlimo, false, false, "Zitrone", false);
+
+        Zutat[] zutatenCaipirinha = {minze, limette, zucker, wasser, eis};
+        Cocktail caipirinha = new Cocktail("Caipirinha", zutatenCaipirinha, false, false, 0, true);
+
+
+
+
+    }
+
 
 }
